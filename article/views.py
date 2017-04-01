2deb4a311896f68d25261e043b922bdaf5aa7d71
yechen <1522120424@qq.com>
2017-04-01 22:36:45 +0800

    确定首页css样式

--- article/views.py
+++ article/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
+import models
 from django.http import HttpResponse
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'index.html', {'hello':'hello blog!'})
+    articles = models.Article.objects.all()
+    return render(request, 'index.html', {'articles': articles})
 
 
